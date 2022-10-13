2702117a0bbe80aa1102c03e4896136952be9377
Jadiha Arul <63629903+jadiha@users.noreply.github.com>
2022-10-13 14:42:43 -0400

    Add files via upload

---
+++ MarkCalculator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace OOP1stProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Console.Write("Hello User! What is your first name?: "); //prompt
+            string name = Console.ReadLine();
+            Console.Write("Hello {0}, what is your last name?: ", name);
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Your full name is {0} {1}", name, lastName);
+            Console.WriteLine("Tell me how old you are and I will double it!");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your aged doubled {0}", age * 2);
+            Console.Write(Console.Read());
+            Console.WriteLine("Please press any key to exit");
+            Console.ReadKey();
+            */
+
+            Console.WriteLine("Hello, thanks for using our program Calculate It! We calulate your individual course average based on the percentages of the assesments that you provide. "); //Program description for user
+
+            Console.WriteLine("----------------------------------------------"); // For visuals
+
+            Console.Write("In order to get started, what is your full name?: "); //Prompts user for name
+            string name = Console.ReadLine();
+
+            // Prompts user for their marks in 5 sections including the weekly activity, assignment 1, assignment 2, midterm and final mark
+            Console.WriteLine("Now, we will be asking you for your marks. Please only enter the digits and not the percentage sign. Have fun! ");
+
+            Console.WriteLine("----------------------------------------------");  // For visuals
+
+            Console.Write("Enter the your weekly activity overall mark: ");
+            double n1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter your assignment 1 mark: ");
+            double n2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter your assignment 2 mark: ");
+            double n3 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter your midterm mark: ");
+            double n4 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter your final mark: ");
+            double n5 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("----------------------------------------------");  // For visuals
+
+            // All marks calculated in one line being multiplied by their respective weighting
+            double result = ((n1 * 0.1) + (n2 * 0.2) + (n3 * 0.2) + (n4 * 0.2) + (n5 * 0.3));
+
+            //Print statement showing user what their calculated mark is 
+            Console.WriteLine("{0}, your calculated mark for the course is {1}%", name, result);
+
+            // User friendliness farewell message 
+            Console.Write("Thanks for using Calculate It! Best of luck in your course, you got this :) ");
+
+        }
+    }
+}
---
+++ TheatreStats.cs
@@ -0,0 +1,248 @@
+﻿using System;
+
+
+namespace TheatreStats
+{
+    class Program { 
+
+        public static void menu()
+        {
+            Console.WriteLine("-----STATS MENU-----");
+            Console.WriteLine("1. Enter theatre information");
+            Console.WriteLine("2. Display theatre information:");
+            Console.WriteLine("3. Quit");
+        }
+
+        static void Main(string[] args)
+        {
+            // declaring variables
+            int[] month = { };
+            int[] monthArray;
+            int[] day = { };
+            int[] dayArray;
+            int[] year = { };
+            int[] yearArray;
+            double[] price = { };
+            double[] priceArray;
+            int[] customers = { };
+            int[] customerArray;
+            int choice;
+            string input_1;
+            string input_2;
+            int input_3;
+            int input_4;
+            int input_5;
+            int input_6;
+            double input_7;
+            string date;
+            string name_pad;
+            string location_pad;
+            int movies;
+            string[] name = { };
+            string[] nameArray;
+            string[] location = { };
+            string[] locationArray;
+            
+
+            DateTime myValue = DateTime.Now;
+            Console.WriteLine(myValue.ToLongDateString());
+            Console.WriteLine(myValue.ToShortTimeString());
+            Console.WriteLine();
+            Console.WriteLine("Welcome to the program TheatreStats! Please select one of the options down below."); //welcome message
+            Console.WriteLine();
+            menu();
+            choice = Convert.ToInt32(Console.ReadLine());
+
+            do
+            { 
+                if (choice == 1)
+                {
+                    Console.WriteLine("What is the theatre name?"); //theatre name
+                    input_1 = Console.ReadLine();
+
+                    while (input_1 == "")
+                    {
+                        Console.WriteLine("This section can not be blank. Please enter respective information. ");
+                        input_1 = Console.ReadLine();
+                    }
+
+
+                    nameArray = new string[name.Length + 1]; // array for theatre names
+
+                    for (int n = 0; n < name.Length; n++) nameArray[n] = name[n];
+
+                    nameArray[nameArray.Length - 1] = input_1;
+                    name = nameArray;
+
+
+
+
+                    Console.WriteLine("What is the theatre location?"); // location
+                    input_2 = Console.ReadLine();
+
+                    while (input_2 == "")
+                    {
+                        Console.WriteLine("This section can not be blank. Please enter respective information");
+                        input_2 = Console.ReadLine();
+                    }
+                    locationArray = new string[location.Length + 1]; //array for location
+
+                    for (int n = 0; n < location.Length; n++) locationArray[n] = location[n];
+
+                    locationArray[locationArray.Length - 1] = input_2;
+                    location = locationArray;
+
+                    //date separated into month, day and year
+
+                    Console.WriteLine("What is the number of the month for you theatre information? ");
+                    input_3 = Convert.ToInt32(Console.ReadLine());
+
+                    while (input_3 < 1 || input_3 > 12)
+                    {
+                        Console.WriteLine("Sorry this is not possible, try again!");
+                        input_3 = Convert.ToInt32(Console.ReadLine());
+                    }
+
+                    monthArray = new int[month.Length + 1]; // array for month
+
+                    for (int n = 0; n < month.Length; n++) monthArray[n] = month[n];
+
+                    monthArray[monthArray.Length - 1] = input_3;
+                    month = monthArray;
+
+                    Console.WriteLine("What is the day?");
+                    input_4 = Convert.ToInt32(Console.ReadLine());
+
+                    while (input_4 <= 0 || input_4 >= 30)
+                    {
+                        Console.WriteLine("Sorry this is not possible, try again!");
+                        input_4 = Convert.ToInt32(Console.ReadLine());
+                    }
+
+                    dayArray = new int[day.Length + 1]; // day array
+
+                    for (int n = 0; n < day.Length; n++) dayArray[n] = day[n];
+
+                    dayArray[dayArray.Length - 1] = input_4;
+                    day = dayArray;
+
+
+                    Console.WriteLine("What is the year?");
+                    input_5 = Convert.ToInt32(Console.ReadLine());
+
+                    while (input_5 < 0 || input_5 > 2021)
+                    {
+                        Console.WriteLine("Sorry this is not possible, try again!");
+                        input_5 = Convert.ToInt32(Console.ReadLine());
+                    }
+
+                    yearArray = new int[year.Length + 1]; // year array
+
+                    for (int n = 0; n < year.Length; n++) yearArray[n] = year[n];
+
+                    yearArray[yearArray.Length - 1] = input_5;
+                    year = yearArray;
+
+
+                    // customers
+
+                    Console.WriteLine("How many people have come to the theatre?");
+                    input_6 = Convert.ToInt32(Console.ReadLine());
+
+                    while (input_6 <= 0)
+                    {
+                        Console.WriteLine("Sorry this is not possible, try again!");
+                        input_6 = Convert.ToInt32(Console.ReadLine());
+                    }
+
+                    customerArray = new int[customers.Length + 1];
+
+                    for (int n = 0; n < customers.Length; n++) customerArray[n] = customers[n];
+                    customerArray[customerArray.Length - 1] = input_6;
+                    customers = customerArray;
+
+                    // price
+
+                    Console.WriteLine("What is the price of the theatre ticket?");
+                    input_7 = Convert.ToDouble(Console.ReadLine());
+
+                    while (input_7 < 9.99 || input_7 > 19.99)
+                    {
+                        Console.WriteLine("Price range must be within $9.99 to $19.99. Please try again!");
+                        input_7 = Convert.ToDouble(Console.ReadLine());
+                    }
+
+                    priceArray = new double[price.Length + 1];
+
+                    for (int n = 0; n < price.Length; n++) priceArray[n] = price[n];
+
+                    priceArray[priceArray.Length - 1] = input_7;
+                    price = priceArray;
+                }
+
+                else if (choice == 2) //displaying info
+                {
+                    if (name.Length == 0) Console.WriteLine("Please enter required information before proceeding!");
+
+                    else
+                    {
+                        Console.WriteLine("Tabular Report");
+                        Console.WriteLine("Theatre Name==Location==Date==Customers==Price");
+
+
+                        for (int n = 0; n < name.Length; n++)
+                        {
+
+                            //name 
+                            name_pad= "";
+
+                            for (int i = 0; i < 20 - name[n].Length; i++) name_pad = name_pad + " ";
+
+                            //location
+                            location_pad = "";
+
+                            for (int i = 0; i < 15 - location[n].Length; i++) location_pad = location_pad + " ";
+
+                            // display
+
+                            date = "";
+
+                            if (month[n] < 10) date = date + " ";
+
+                            if (day[n] < 10) date = date + " ";
+                            date = date + month[n] + "/" + day[n] + "/" + year[n];
+
+                            Console.WriteLine(name[n] + name_pad + location[n] + location_pad + "" + date + "" + customers[n] + "$" + price[n]);
+
+                        }
+
+                        for (int n = 0; n < location.Length; n++) ;
+
+                        for (int n = 0; n < month.Length; n++) ;
+
+                        for (int n = 0; n < day.Length; n++) ;
+
+                        for (int n = 0; n < year.Length; n++) ;
+
+                        for (int n = 0; n < customers.Length; n++) ;
+
+                        for (int n = 0; n < price.Length; n++) ;
+
+
+                        movies = name.Length;
+                        Console.WriteLine("You are seeing " + movies + "  movies");
+                        break;
+
+                    }
+
+
+                }
+                else if (choice != 3) Console.WriteLine("Invalid entry!");
+            }
+            while (choice != 3);
+            Console.WriteLine("Thanks for using Theatre Stats! Goodbye! ");
+
+        }
+    }
+}
+         
